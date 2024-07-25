38a92d315dece9ba508580568f403dc6419c59c3
Young5097 <bigfaith0301@naver.com>
2024-07-25 18:40:52 +0900

    fix : 로그인오류수정 + 일부기능추가
    - 401 status 처리
    - 로그인 실패시 입력폼 초기화 및 실패메세지 하단에 뛰우기
    - Navigate로 회원가입 버튼 클릭시 signup페이지로 이동

--- src/pages/login/LoginComponent.js
+++ src/pages/login/LoginComponent.js
@@ -1,12 +1,15 @@
-import { redirect, useActionData, Form } from 'react-router-dom';
+import { redirect, useActionData, Form, useNavigate } from 'react-router-dom';
 import "./LoginComponent.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function LoginComponent() {
     const data = useActionData();
-    console.log("useActionData: ", data);
+    const navigate = useNavigate();
 
+    const [loginId, setLoginId] = useState("");
+    const [pw, setPw] = useState("");
     const [passwordVisible, setPasswordVisible] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     // 비밀번호 표시/숨기기 함수
     const togglePasswordVisibility = () => {
@@ -14,8 +17,20 @@ function LoginComponent() {
     };
 
     const handleRedirectHome = () => {
-        window.location.href = "/eDrink24"
+        window.location.href = "/eDrink24";
+    };
+
+    const handleDirectNormalSignup = () => {
+        navigate("/eDrink24/signup")
     }
+
+    useEffect(() => {
+        if (data?.error) {
+            setErrorMessage(data.error);
+            setLoginId("");
+            setPw("");
+        }
+    }, [data]);
 
     return (
         <div className="login-container">
@@ -30,13 +45,30 @@ function LoginComponent() {
                 <h1>환영합니다 : )</h1>
             </div>
             <Form method="post">
-                <input type="text" name="loginId" className="login-input" placeholder="아이디를 입력해 주세요" required />
+                <input
+                    type="text"
+                    name="loginId"
+                    className="login-input"
+                    placeholder="아이디를 입력해 주세요"
+                    value={loginId}
+                    onChange={(e) => setLoginId(e.target.value)}
+                    required
+                />
                 <div className="password-container">
-                    <input type={passwordVisible ? "text" : "password"} name="pw" className="password-input" placeholder="비밀번호를 입력해 주세요" required />
+                    <input
+                        type={passwordVisible ? "text" : "password"}
+                        name="pw"
+                        className="password-input"
+                        placeholder="비밀번호를 입력해 주세요"
+                        value={pw}
+                        onChange={(e) => setPw(e.target.value)}
+                        required
+                    />
                     <button type="button" className="show-password" onClick={togglePasswordVisibility}>
                         <img src="assets/login/eye-open.png" alt="hide 버튼" />
                     </button>
                 </div>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
                 <button type="submit" className="login-button">로그인</button>
             </Form>
             <div className="options">
@@ -45,7 +77,7 @@ function LoginComponent() {
             </div>
             <div className="signup-options">
                 <p>혹시, 계정이 없으신가요?</p>
-                <button className="normal-signup">
+                <button className="normal-signup" onClick={handleDirectNormalSignup}>
                     <span>일반회원으로 가입하기</span>
                 </button>
                 <button className="google-signup">
@@ -77,6 +109,14 @@ export async function action({ request }) {
         body: JSON.stringify(authData)
     });
     console.log("로그인 요청결과: ", response);
+
+    if (!response.ok) {
+        if (response.status === 401) {
+            return { error: "아이디 또는 패스워드가 잘못되었습니다." };
+        } else {
+            return { error: "서버에 문제가 발생했습니다. 나중에 다시 시도해 주세요." };
+        }
+    }
 
     const resData = await response.json();
     console.log("resData: ", resData);
