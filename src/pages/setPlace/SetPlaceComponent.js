f7741bb393428f7ac49ebc372e650c54215d55ce
Young5097 <bigfaith0301@naver.com>
2024-08-14 13:32:11 +0900

    [feature] 매장위치 마커찍기
    - 지도 center 기준 1km 내의 매장들의 위치를 마커로 찍어줌
    - 매장위치는 서버에서 받아오면 부하가 심해서 react에 json으로 저장해놓고 사용
    - fetchAddressToLL를 통해서 위도, 경도를 넣은 json 파일을 업데이터 해서 사용
    - 향 후 완료버튼을 통해 사용자 데이터를 업데이터 해줘야함

--- src/pages/setPlace/SetPlaceComponent.js
+++ src/pages/setPlace/SetPlaceComponent.js
@@ -1,83 +1,122 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import FooterComponent from '../../components/footer/FooterComponent.js';
-
 import './SetPlaceComponent.css';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk';
 
 function SetPlaceComponent() {
     const navigate = useNavigate();
     const location = useLocation();
     const { customerData } = location.state || {};
 
-    //-----------------------------------------------------------------------------------------
     const geocoder = new window.kakao.maps.services.Geocoder();
 
-    var currentLocation = customerData.currentLocation; // CUSTOMER.currentLocation
-    var currentStore = customerData.currentStore; // CUSTOMER.currentStore
+    const currentLocation = customerData.currentLocation;
+    const currentStore = customerData.currentStore;
 
-    const [myLocation, setMyLocation] = useState(null); // 위도, 경도
-    const [myAddress, setMyAddress] = useState(""); // 도로명주소
+    const [locationData, setLocationData] = useState({
+        latitude: null,
+        longitude: null,
+        address: ""
+    });
 
+    const [stores, setStores] = useState([]); // 매장 json
+    const [storeMarkers, setStoreMarkers] = useState([]); // 필터된 매장 위치
 
-    // 페이지 랜더링시, 가져오는 위치
+    const [choiceStore, setChoiceStore] = useState(null);
+    const [openInfo, setOpenInfo] = useState(null);
+
+    // 페이지 랜더링 시, 가져오는 위치
     useEffect(() => {
-        if (currentLocation) {
-            fetchAddressToLL(currentLocation);
-        } else {
-            fetchCurrentLocation();
-        }
+        const fetchInit = async () => {
+            if (currentLocation) {
+                fetchAddressToLL(currentLocation);
+            } else {
+                fetchCurrentLocation();
+            }
+            try {
+                const response = await fetch('assets/store/store_with_latlng.json');
+                const store_data = await response.json();
+                setStores(store_data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchInit();
     }, []);
 
-    // 패이지 랜더링시, 주소 가져오기
+    // 위치 데이터 변경 시, 주소 가져오기
     useEffect(() => {
-        if (myLocation) {
-            fetchLLToAddress(myLocation);
+        if (locationData.latitude && locationData.longitude) {
+            fetchLLToAddress(locationData.latitude, locationData.longitude);
         }
-    }, [myLocation]);
-
+    }, [locationData.latitude, locationData.longitude]);
 
     // 주소 => 위경도
     const fetchAddressToLL = (address) => {
-        geocoder.addressSearch(address, (result, status) => { // result => 0 : {..., x, y,...}
+        geocoder.addressSearch(address, (result, status) => {
             if (status === window.kakao.maps.services.Status.OK) {
-                setMyLocation({ latitude: result[0].y, longitude: result[0].x });
-            } else {
-                console.error(status);
-            }
-        })
-    };
-
-    // 위경도 => 주소
-    const fetchLLToAddress = (location) => {
-        const coord = new window.kakao.maps.LatLng(location.latitude, location.longitude);
-        geocoder.coord2Address(coord.getLng(), coord.getLat(), (result, status) => {
-            if (status === window.kakao.maps.services.Status.OK) {
-                setMyAddress(result[0].address.address_name);
+                setLocationData({
+                    latitude: result[0].y,
+                    longitude: result[0].x,
+                    address: address
+                });
             } else {
                 console.error(status);
             }
         });
     };
 
-    // 본인주소 기준 위치찾기
+    // 위경도 => 주소
+    const fetchLLToAddress = (latitude, longitude) => {
+        const latlng = new window.kakao.maps.LatLng(latitude, longitude);
+        geocoder.coord2Address(latlng.getLng(), latlng.getLat(), (result, status) => {
+            if (status === window.kakao.maps.services.Status.OK) {
+                setLocationData(prevData => ({
+                    ...prevData,
+                    address: result[0].address.address_name
+                }));
+            } else {
+                console.error(status);
+            }
+        });
+    };
+
+    // 본인주소 기준 위치 찾기
     const fetchAddressLocation = () => {
         fetchAddressToLL(currentLocation);
         navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
-    }
-    // 현재 본인위치 기준 위치찾기
+    };
+
+    // 현재 본인위치 기준 위치 찾기
     const fetchCurrentLocation = () => {
-        setMyLocation(null);
+        setLocationData({ latitude: null, longitude: null, address: "" });
         navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
     };
 
     const successHandler = (response) => {
         const { latitude, longitude } = response.coords;
-        setMyLocation({ latitude, longitude });
+        setLocationData({ latitude, longitude, address: locationData.address });
     };
 
     const errorHandler = (error) => {
         console.log(error);
+    };
+
+    // 반경 내의 마커 필터링
+    const filterMarker = (map) => {
+        const radius = 1000; // 1km 반경
+        const center = map.getCenter();
+        const filteredMarkers = stores.map((store) => {
+            const position = new window.kakao.maps.LatLng(store.latitude, store.longitude);
+            const polyline = new window.kakao.maps.Polyline({
+                path: [center, position]
+            });
+            const distance = polyline.getLength();
+
+            return distance < radius ? { ...store, visible: true } : { ...store, visible: false };
+        });
+        setStoreMarkers(filteredMarkers);
     };
 
     return (
@@ -91,13 +130,59 @@ function SetPlaceComponent() {
 
             <div className="setPlace-body">
                 <div className="setPlace-map">
-                    {myLocation && (
+                    {locationData.latitude && locationData.longitude && (
                         <Map
-                            center={{ lat: myLocation.latitude, lng: myLocation.longitude }}
-                            style={{ width: '390px', height: '390px' }}
-                            level={5}
+                            center={{ lat: locationData.latitude, lng: locationData.longitude }}
+                            style={{ width: '390px', height: '490px' }}
+                            level={4}
+
+                            onIdle={filterMarker}
                         >
-                            <MapMarker position={{ lat: myLocation.latitude, lng: myLocation.longitude }} />
+                            <MapMarker position={{ lat: locationData.latitude, lng: locationData.longitude }} />
+                            {storeMarkers.map((store) => (
+
+                                store.visible && (
+                                    <MapMarker
+                                        key={store.storeId}
+                                        position={{ lat: store.latitude, lng: store.longitude }}
+                                        title={store.storeName}
+                                        clickable={true}
+                                        image={{
+                                            src: "assets/store/marker_store.png",
+                                            size: {
+                                                width: 24,
+                                                height: 24,
+                                            },
+                                        }}
+
+                                        onClick={() => setOpenInfo(store.storeId)}
+                                    >
+                                        {openInfo === store.storeId && (
+                                            <div style={{ minWidth: "150px" }}>
+                                                <img
+                                                    alt="close"
+                                                    width="14"
+                                                    height="13"
+                                                    src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+                                                    style={{
+                                                        position: "absolute",
+                                                        right: "5px",
+                                                        top: "5px",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    onClick={() => setOpenInfo(null)}
+                                                />
+                                                <div style={{ padding: "5px", color: "#000" }}>
+                                                    <h4>{store.storeName}</h4>
+                                                    <p>{store.storeAddress}</p>
+                                                    <p>{store.storePhoneNum}</p>
+                                                    <button onClick={() => setChoiceStore(store)}>매장 선택</button>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </MapMarker>
+                                )
+                            ))}
                         </Map>
                     )}
                 </div>
@@ -108,14 +193,19 @@ function SetPlaceComponent() {
                 </div>
 
                 <div className='setPlace-showAddress'>
-                    <p>
-                        {myAddress}
-                    </p>
+                    <p>내 위치 : {locationData.address}</p>
+
+                    {choiceStore && (
+                        <div className="chosen-store">
+                            <p>선택한 매장 : {choiceStore.storeName}</p>
+                            <p>주소 : {choiceStore.storeAddress}</p>
+                            <p>전화번호 : {choiceStore.storePhoneNum}</p>
+                        </div>
+                    )}
                 </div>
 
                 <div className="setPlace-update-button">
                     <button className='setPlace-set-btn'>단골매장 설정하기</button>
-
                 </div>
             </div>
 
@@ -128,4 +218,5 @@ export default SetPlaceComponent;
 
 /*
     https://apis.map.kakao.com/web/documentation/#services_Geocoder
+    https://react-kakao-maps-sdk.jaeseokim.dev/
 */
