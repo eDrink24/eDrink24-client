bdf77578047c564faa10d3452b161e764d031334
giuk-kim2 <gimgiug87@gmail.com>
2024-08-14 17:57:57 +0900

    [fix] price
    -  제품 가격에 000가 형성될때마다  "," 추가해주기

--- src/pages/product/ProductDetailComponent.js
+++ src/pages/product/ProductDetailComponent.js
@@ -213,7 +213,7 @@ function ProductDetailComponent() {
         </div>
 
         <div className="productDetailComponent-price-per">
-          <span className="productDetailComponent-price-item">{product.price} 원</span>
+          <span className="productDetailComponent-price-item">{Number(product.price).toLocaleString()} 원</span>
         </div>
 
         <img className="productDetailComponent-today-pickup-img"
@@ -271,13 +271,13 @@ function ProductDetailComponent() {
                 <button className="productDetailComponent-quantity-button" onClick={increaseQuantity}>+</button>
               </div>
               <div className="productDetailComponent-price-per-info">
-                <span className="productDetailComponent-price-per-item">{product.price}원</span>
+                <span className="productDetailComponent-price-per-item">{Number(product.price).toLocaleString()}원</span>
               </div>
             </div>
 
             <div className="productDetailComponent-total-info">
               <span className="productDetailComponent-total-quantity">총 수량: {quantity}개</span>
-              <span className="productDetailComponent-total-price">총 가격: {product.price * quantity}원</span>
+              <span className="productDetailComponent-total-price">총 가격: {Number(product.price).toLocaleString() * quantity}원</span>
             </div>
 
           </div>
