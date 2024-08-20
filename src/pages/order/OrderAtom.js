0738db71c82934d0268be7a788fe59486ec91e28
giuk-kim2 <gimgiug87@gmail.com>
2024-08-20 18:33:55 +0900

    [fix] 바로구매 버튼 수정
    - 제품상세보기 페이지에서 바로 구매 클릭 시
      제품 정보와 함께 픽업 유형도 나오도록 수정

--- src/pages/order/OrderAtom.js
+++ src/pages/order/OrderAtom.js
@@ -6,12 +6,13 @@ export const orderState = atom({
     basket: [],
     storeId: null,
     pickupDate: null,
+    pickupType: null,
     coupon: null,
     pointUse: false,
     paymentMethod: '',
     totalPrice: 0,
     discount: 0,
     finalAmount: 0,
-    selectedItems: [] // 바로 구매 버튼을 눌렀을 때 선택된 상품의 정보가 임시로 저장
+    selectedItems: [], // 바로 구매 버튼을 눌렀을 때 선택된 상품의 정보가 임시로 저장
   },
 });
